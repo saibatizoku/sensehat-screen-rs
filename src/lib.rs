5afe206072462e01f50a07112b59b24c6ea2370e
Joaquin Rosales <globojorro@gmail.com>
2018-03-31 22:28:26 -0600

    ColorPixel::rgb565 returns [lo, hi], and we test it

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,9 @@ impl PixelColor {
 
     /// Create a new LED pixel color from a pair of RGB565-encoded bytes.
     pub fn from_rgb565(color: [u8; 2]) -> Self {
-        let red = ((color[0] >> 3) & 0x1F) << 3;
-        let green = (color[0] & 0b0000_0111) << 5 | (color[1] & 0b1110_0000) >> 3;
-        let blue = (color[1] & 0b0001_1111) << 3;
+        let red = ((color[1] >> 3) & 0x1F) << 3;
+        let green = (color[1] & 0b0000_0111) << 5 | (color[0] & 0b1110_0000) >> 3;
+        let blue = (color[0] & 0b0001_1111) << 3;
         PixelColor::new(red, green, blue)
     }
 
@@ -31,7 +31,7 @@ impl PixelColor {
         let rgb = (r << 11) + (g << 5) + b;
         let lsb = (rgb & 0x00FF) as u8;
         let msb = (rgb.swap_bytes() & 0x00FF) as u8;
-        [msb, lsb]
+        [lsb, msb]
     }
 }
 
@@ -53,23 +53,26 @@ mod tests {
         );
         // 100% green - 6-bit resolution
         assert_eq!(
-            PixelColor::from_rgb565([0x07, 0xE0]),
+            PixelColor::from_rgb565([0xE0, 0x07]),
             PixelColor::new(0x00, 0xFC, 0x00)
         );
     }
 
+    // TODO: x86 linux stores `WORDS` as little-endian, meaning the low-byte is
+    // sent before the high-byte. Change dealing with [u8; 2] and replace with a
+    // single u16.
     #[test]
     fn color_pixel_converts_rgb_into_2_bytes_rgb565() {
         let white_pixel = PixelColor::new(0xFF, 0xFF, 0xFF);
         assert_eq!(white_pixel.rgb565(), [0xFF, 0xFF]);
 
         let red_pixel = PixelColor::new(0xFF, 0x00, 0x00);
-        assert_eq!(red_pixel.rgb565(), [0xF8, 0x00]);
+        assert_eq!(red_pixel.rgb565(), [0x00, 0xF8]);
 
         let green_pixel = PixelColor::new(0x00, 0xFF, 0x00);
-        assert_eq!(green_pixel.rgb565(), [0x07, 0xE0]);
+        assert_eq!(green_pixel.rgb565(), [0xE0, 0x07]);
 
         let blue_pixel = PixelColor::new(0x00, 0x00, 0xFF);
-        assert_eq!(blue_pixel.rgb565(), [0x00, 0x1F]);
+        assert_eq!(blue_pixel.rgb565(), [0x1F, 0x00]);
     }
 }
