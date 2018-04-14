24d24c503e036a7f5a2f8ac7f6a087e499bcd2a8
Joaquin Rosales <globojorro@gmail.com>
2018-04-14 00:21:40 -0500

    add rotation tests for new PixelFrame type

--- src/frame.rs
+++ src/frame.rs
@@ -50,6 +50,53 @@ impl Default for FrameLine {
 mod tests {
     use super::*;
 
+    const DARK: PixelColor = PixelColor::BLACK;
+    const BLUE: PixelColor = PixelColor::BLUE;
+
+    const CHECKER_BASE: [PixelColor; 64] = [
+        DARK, DARK, DARK, DARK, BLUE, BLUE, BLUE, BLUE, //
+        DARK, DARK, DARK, DARK, BLUE, BLUE, BLUE, BLUE, //
+        DARK, DARK, BLUE, DARK, BLUE, DARK, DARK, DARK, //
+        DARK, DARK, DARK, DARK, BLUE, DARK, DARK, DARK, //
+        DARK, DARK, DARK, DARK, DARK, DARK, DARK, DARK, //
+        DARK, DARK, DARK, DARK, DARK, DARK, BLUE, DARK, //
+        BLUE, DARK, DARK, DARK, DARK, DARK, BLUE, DARK, //
+        BLUE, BLUE, BLUE, DARK, DARK, DARK, DARK, DARK, //
+    ];
+
+    const CHECKER_90_CW: [PixelColor; 64] = [
+        BLUE, BLUE, DARK, DARK, DARK, DARK, DARK, DARK, //
+        BLUE, DARK, DARK, DARK, DARK, DARK, DARK, DARK, //
+        BLUE, DARK, DARK, DARK, DARK, BLUE, DARK, DARK, //
+        DARK, DARK, DARK, DARK, DARK, DARK, DARK, DARK, //
+        DARK, DARK, DARK, DARK, BLUE, BLUE, BLUE, BLUE, //
+        DARK, DARK, DARK, DARK, DARK, DARK, BLUE, BLUE, //
+        DARK, BLUE, BLUE, DARK, DARK, DARK, BLUE, BLUE, //
+        DARK, DARK, DARK, DARK, DARK, DARK, BLUE, BLUE, //
+    ];
+
+    const CHECKER_180: [PixelColor; 64] = [
+        DARK, DARK, DARK, DARK, DARK, BLUE, BLUE, BLUE, //
+        DARK, BLUE, DARK, DARK, DARK, DARK, DARK, BLUE, //
+        DARK, BLUE, DARK, DARK, DARK, DARK, DARK, DARK, //
+        DARK, DARK, DARK, DARK, DARK, DARK, DARK, DARK, //
+        DARK, DARK, DARK, BLUE, DARK, DARK, DARK, DARK, //
+        DARK, DARK, DARK, BLUE, DARK, BLUE, DARK, DARK, //
+        BLUE, BLUE, BLUE, BLUE, DARK, DARK, DARK, DARK, //
+        BLUE, BLUE, BLUE, BLUE, DARK, DARK, DARK, DARK, //
+    ];
+
+    const CHECKER_90_CCW: [PixelColor; 64] = [
+        BLUE, BLUE, DARK, DARK, DARK, DARK, DARK, DARK, //
+        BLUE, BLUE, DARK, DARK, DARK, BLUE, BLUE, DARK, //
+        BLUE, BLUE, DARK, DARK, DARK, DARK, DARK, DARK, //
+        BLUE, BLUE, BLUE, BLUE, DARK, DARK, DARK, DARK, //
+        DARK, DARK, DARK, DARK, DARK, DARK, DARK, DARK, //
+        DARK, DARK, BLUE, DARK, DARK, DARK, DARK, BLUE, //
+        DARK, DARK, DARK, DARK, DARK, DARK, DARK, BLUE, //
+        DARK, DARK, DARK, DARK, DARK, DARK, BLUE, BLUE, //
+    ];
+
     #[test]
     fn frame_line_is_created_from_slice_of_bytes() {
         let green: [u8; 8] = [0xE0, 0x07, 0xE0, 0x07, 0xE0, 0x07, 0xE0, 0x07];
@@ -71,5 +118,33 @@ mod tests {
         let blue = PixelColor::from_rgb565([0x00, 0x1F]);
         let frame_line = FrameLine::from_pixels(&[blue, blue]);
         assert_eq!(frame_line.as_slice(), &[0x00, 0x1F, 0x00, 0x1F]);
+    }
+
+    #[test]
+    fn pixel_frame_is_rotated_90_degrees_left() {
+        let checker_base = PixelFrame(CHECKER_BASE.to_vec());
+        let checker_left = PixelFrame(CHECKER_90_CCW.to_vec());
+        assert_eq!(checker_base.rotate_left(), checker_left);
+    }
+
+    #[test]
+    fn pixel_frame_is_rotated_by_180_degrees() {
+        let checker_base = PixelFrame(CHECKER_BASE.to_vec());
+        let checker_180 = PixelFrame(CHECKER_180.to_vec());
+        assert_eq!(checker_base.rotate_180(), checker_180);
+    }
+
+    #[test]
+    fn pixel_frame_is_rotated_by_180_degrees_by_two_90_deg_steps() {
+        let checker_base = PixelFrame(CHECKER_BASE.to_vec());
+        let checker_180 = PixelFrame(CHECKER_180.to_vec());
+        assert_eq!(checker_base.rotate_left().rotate_left(), checker_180);
+    }
+
+    #[test]
+    fn pixel_frame_is_rotated_90_degrees_right() {
+        let checker_base = PixelFrame(CHECKER_BASE.to_vec());
+        let checker_right = PixelFrame(CHECKER_90_CW.to_vec());
+        assert_eq!(checker_base.rotate_right(), checker_right);
     }
 }
