7a714c88083b257df0b39a8d6710cfb03d0cbce2
Joaquin Rosales <globojorro@gmail.com>
2018-04-26 18:43:33 -0500

    add 'clip' feature to crate; add FrameClip documentation

--- src/frame_clip.rs
+++ src/frame_clip.rs
@@ -1,7 +1,12 @@
-//! Support for making clips out of two `PixelFrame`s.
+//! Support for combining `PixelFrame`s.
+//!
+//! The `FrameClip` is the type that merges `PixelFrame` by rows or by columns
+//!
+//!
 use super::PixelFrame;
 use super::offset::Offset;
 
+/// Methods enabled by the `clip` feature.
 impl PixelFrame {
     /// Create a `FrameClip` with this and another `PixelFrame`.
     pub fn clip(&self, other: &PixelFrame) -> FrameClip {
@@ -10,6 +15,267 @@ impl PixelFrame {
 }
 
 /// A clip made of two `PixelFrame`s.
+/// # Horizontal `FrameClip`
+///
+/// In horizontal clips, frames are placed side-by-side, and a `FrameClip` is specified
+/// using either `Offset::Right(_)`, or `Offset::Left(_)` with the `FrameClip::offset` method.
+///
+///
+/// `Offset::Right(_)`
+/// ------------------
+///
+/// ```text
+///                 |---------------|
+///                 |    Right(8)   |
+///                 |---------------|
+///               |---------------| |
+///               |    Right(7)   | |
+///               |---------------| |
+///             |---------------| | |
+///             |    Right(6)   | | |
+///             |---------------| | |
+///           |---------------| | | |
+///           |    Right(5)   | | | |
+///           |---------------| | | |
+///         |---------------| | | | |
+///         |    Right(4)   | | | | |
+///         |---------------| | | | |
+///       |---------------| | | | | |
+///       |    Right(3)   | | | | | |
+///       |---------------| | | | | |
+///     |---------------| | | | | | |
+///     |    Right(2)   | | | | | | |
+///     |---------------| | | | | | |
+///   |---------------| | | | | | | |
+///   |    Right(1)   | | | | | | | |
+///   |---------------| | | | | | | |
+/// |---------------| | | | | | | | |
+/// |    Right(0)   | | | | | | | | |
+/// |---------------| | | | | | | | |
+///                 | | | | | | | | |
+/// |0 1 2 3 4 5 6 7|0 1 2 3 4 5 6 7|  <- individual frame COLUMN index
+/// |---------------|---------------|
+/// |    frame 1    |    frame 2    |
+/// |---------------|---------------|
+/// ```
+///
+/// ### Example
+///
+/// ```rust
+/// extern crate sensehat_screen;
+/// use sensehat_screen::{FrameClip, PixelColor, PixelFrame};
+/// use sensehat_screen::frame::offset::Offset;
+///
+/// fn main() {
+///     let frame_1 = PixelFrame::new(&[PixelColor::YELLOW; 64]);
+///     let frame_2 = PixelFrame::new(&[PixelColor::BLUE; 64]);
+///     let clip = FrameClip::new(frame_1.clone(), frame_2.clone());
+///     // Offset of `0`, shows the first frame.
+///     assert_eq!(clip.offset(Offset::Right(0)), frame_1);
+///
+///     let offset_2_cols = clip.offset(Offset::Right(2)).as_columns();
+///     // expect 2 yellow columns
+///     assert_eq!(&offset_2_cols[..2], &[&[PixelColor::BLUE; 8]; 2]);
+///     // expect 6 blue columns
+///     assert_eq!(&offset_2_cols[2..], &[&[PixelColor::YELLOW; 8]; 6]);
+///
+///     // Offset of `8`, shows the second frame.
+///     assert_eq!(clip.offset(Offset::Right(8)), frame_2);
+/// }
+/// ```
+/// `Offset::Left(_)`
+/// ----------------
+/// ```text
+///                 |---------------|
+///                 |    Left(0)    |
+///                 |---------------|
+///               |---------------| |
+///               |    Left(1)    | |
+///               |---------------| |
+///             |---------------| | |
+///             |    Left(2)    | | |
+///             |---------------| | |
+///           |---------------| | | |
+///           |    Left(3)    | | | |
+///           |---------------| | | |
+///         |---------------| | | | |
+///         |    Left(4)    | | | | |
+///         |---------------| | | | |
+///       |---------------| | | | | |
+///       |    Left(5)    | | | | | |
+///       |---------------| | | | | |
+///     |---------------| | | | | | |
+///     |    Left(6)    | | | | | | |
+///     |---------------| | | | | | |
+///   |---------------| | | | | | | |
+///   |    Left(7)    | | | | | | | |
+///   |---------------| | | | | | | |
+/// |---------------| | | | | | | | |
+/// |    Left(8)    | | | | | | | | |
+/// |---------------| | | | | | | | |
+///                 | | | | | | | | |
+/// |0 1 2 3 4 5 6 7|0 1 2 3 4 5 6 7|  <- individual frame COLUMN index
+/// |---------------|---------------|
+/// |      frame 2  |      frame 1  |
+/// |---------------|---------------|
+///
+/// ```
+///
+/// ### Example
+///
+/// ```rust
+/// extern crate sensehat_screen;
+/// use sensehat_screen::{FrameClip, PixelColor, PixelFrame};
+/// use sensehat_screen::frame::offset::Offset;
+///
+/// fn main() {
+///     let frame_1 = PixelFrame::new(&[PixelColor::YELLOW; 64]);
+///     let frame_2 = PixelFrame::new(&[PixelColor::BLUE; 64]);
+///     let clip = FrameClip::new(frame_1.clone(), frame_2.clone());
+///     // Offset of `0`, shows the first frame.
+///     assert_eq!(clip.offset(Offset::Left(0)), frame_1);
+///
+///     let offset_4_cols = clip.offset(Offset::Left(4)).as_columns();
+///     // expect 4 yellow columns
+///     assert_eq!(&offset_4_cols[..4], &[&[PixelColor::YELLOW; 8]; 4]);
+///     // expect 4 blue columns
+///     assert_eq!(&offset_4_cols[4..], &[&[PixelColor::BLUE; 8]; 4]);
+///
+///     // Offset of `8`, shows the second frame.
+///     assert_eq!(clip.offset(Offset::Left(8)), frame_2);
+/// }
+/// ```
+///
+/// Vertical `FrameClip`
+/// ====================
+///
+/// In vertical clips, frames are placed above-and-belowe each other, and a `FrameClip` is specified
+/// using either `Offset::Bottom(_)`, or `Offset::Top(_)` with the `FrameClip::offset` method.
+///
+/// `Offset::Bottom(_)`
+/// -------------------
+/// ```text
+///                 |---------------|
+///                 |   Bottom(0)   |
+///                 |---------------|
+///               |---------------| |
+///               |   Bottom(1)   | |
+///               |---------------| |
+///             |---------------| | |
+///             |   Bottom(2)   | | |
+///             |---------------| | |
+///           |---------------| | | |
+///           |   Bottom(3)   | | | |
+///           |---------------| | | |
+///         |---------------| | | | |
+///         |   Bottom(4)   | | | | |
+///         |---------------| | | | |
+///       |---------------| | | | | |
+///       |   Bottom(5)   | | | | | |
+///       |---------------| | | | | |
+///     |---------------| | | | | | |
+///     |   Bottom(6)   | | | | | | |
+///     |---------------| | | | | | |
+///   |---------------| | | | | | | |
+///   |   Bottom(7)   | | | | | | | |
+///   |---------------| | | | | | | |
+/// |---------------| | | | | | | | |
+/// |   Bottom(8)   | | | | | | | | |
+/// |---------------| | | | | | | | |
+///                 | | | | | | | | |
+/// |0 1 2 3 4 5 6 7|0 1 2 3 4 5 6 7|  <- individual frame ROW index
+/// |---------------|---------------|
+/// |      frame 2  |      frame 1  |
+/// |---------------|---------------|
+///
+/// ```
+///
+/// ### Example
+///
+/// ```rust
+/// extern crate sensehat_screen;
+/// use sensehat_screen::{FrameClip, PixelColor, PixelFrame};
+/// use sensehat_screen::frame::offset::Offset;
+///
+/// fn main() {
+///     let frame_1 = PixelFrame::new(&[PixelColor::MAGENTA; 64]);
+///     let frame_2 = PixelFrame::new(&[PixelColor::WHITE; 64]);
+///     let clip = FrameClip::new(frame_1.clone(), frame_2.clone());
+///     // Offset of `0`, shows the first frame.
+///     assert_eq!(clip.offset(Offset::Bottom(0)), frame_1);
+///
+///     let offset_2_rows = clip.offset(Offset::Bottom(2)).as_rows();
+///     // expect 2 yellow rows
+///     assert_eq!(&offset_2_rows[..2], &[&[PixelColor::WHITE; 8]; 2]);
+///     // expect 6 blue rows
+///     assert_eq!(&offset_2_rows[2..], &[&[PixelColor::MAGENTA; 8]; 6]);
+///
+///     // Offset of `8`, shows the second frame.
+///     assert_eq!(clip.offset(Offset::Bottom(8)), frame_2);
+/// }
+/// ```
+///
+/// `Offset::Top(_)`
+/// ----------------
+/// ```text
+///                 |---------------|
+///                 |    Top(8)     |
+///                 |---------------|
+///               |---------------| |
+///               |    Top(7)     | |
+///               |---------------| |
+///             |---------------| | |
+///             |    Top(6)     | | |
+///             |---------------| | |
+///           |---------------| | | |
+///           |    Top(5)     | | | |
+///           |---------------| | | |
+///         |---------------| | | | |
+///         |    Top(4)     | | | | |
+///         |---------------| | | | |
+///       |---------------| | | | | |
+///       |    Top(3)     | | | | | |
+///       |---------------| | | | | |
+///     |---------------| | | | | | |
+///     |    Top(2)     | | | | | | |
+///     |---------------| | | | | | |
+///   |---------------| | | | | | | |
+///   |    Top(1)     | | | | | | | |
+///   |---------------| | | | | | | |
+/// |---------------| | | | | | | | |
+/// |    Top(0)     | | | | | | | | |
+/// |---------------| | | | | | | | |
+///                 | | | | | | | | |
+/// |0 1 2 3 4 5 6 7|0 1 2 3 4 5 6 7|  <- individual frame ROW index
+/// |---------------|---------------|
+/// |    frame 1    |    frame 2    |
+/// |---------------|---------------|
+/// ```
+///
+/// ### Example
+///
+/// ```rust
+/// extern crate sensehat_screen;
+/// use sensehat_screen::{FrameClip, PixelColor, PixelFrame};
+/// use sensehat_screen::frame::offset::Offset;
+///
+/// fn main() {
+///     let frame_1 = PixelFrame::new(&[PixelColor::GREEN; 64]);
+///     let frame_2 = PixelFrame::new(&[PixelColor::BLACK; 64]);
+///     let clip = FrameClip::new(frame_1.clone(), frame_2.clone());
+///     // Offset of `0`, shows the first frame.
+///     assert_eq!(clip.offset(Offset::Top(0)), frame_1);
+///
+///     let offset_7_rows = clip.offset(Offset::Top(7)).as_rows();
+///     // expect 1 green rows
+///     assert_eq!(&offset_7_rows[..1], &[&[PixelColor::GREEN; 8]; 1]);
+///     // expect 7 blue rows
+///     assert_eq!(&offset_7_rows[1..], &[&[PixelColor::BLACK; 8]; 7]);
+///
+///     // Offset of `8`, shows the second frame.
+///     assert_eq!(clip.offset(Offset::Left(8)), frame_2);
+/// }
+/// ```
 #[derive(Clone, Debug, Default)]
 pub struct FrameClip(PixelFrame, PixelFrame);
 
